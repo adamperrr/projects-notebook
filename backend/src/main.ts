6b68ae984e27153bc6063e7c51239b3a553d428b
Adam <qadam.pertek@poczta.fm>
2022-03-29 12:00:34 +0200

    [backend] feat: add api versioning in path

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from "@nestjs/common";
+import { ValidationPipe, VersioningType } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
@@ -6,6 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
 
   await app.listen(8000);
 }
