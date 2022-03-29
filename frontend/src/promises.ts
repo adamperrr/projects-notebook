d4c77b899f725e382f9e9e6663a1018149904f4a
Adam <qadam.pertek@poczta.fm>
2022-03-29 23:19:09 +0200

    [backend] feat: separate components

--- frontend/src/promises.ts
+++ frontend/src/promises.ts
@@ -1,6 +1,6 @@
 import { API_URL } from "./constants";
 
-export const getMonthDays = (year: number, month: number) => {
-  const url = `${API_URL}/v1/calendar-day/${year}/${month}`;
+export const getMonthDays = (year: number, monthNumber: number) => {
+  const url = `${API_URL}/v1/calendar-day/${year}/${monthNumber}`;
   return fetch(url).then((res) => res.json());
 };
